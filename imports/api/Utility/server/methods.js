624d6110f2498b1c1b4fc4cf575114155f0dba55
Danny <DJ@MacBook-Pro.local>
2017-10-09 23:43:12 -0600

    initial push

--- imports/api/Utility/server/methods.js
+++ imports/api/Utility/server/methods.js
@@ -2,10 +2,22 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import getPrivateFile from '../../../modules/server/get-private-file';
 import parseMarkdown from '../../../modules/parse-markdown';
+const PythonShell = require('python-shell');
 
 Meteor.methods({
   'utility.getPage': function utilityGetPage(fileName) {
     check(fileName, String);
     return parseMarkdown(getPrivateFile(`pages/${fileName}.md`));
   },
+  'utility.imageAI': function utilityImageAI(image) {
+  	console.log(image)
+	var options = {
+    	args: [image]
+}
+	
+	PythonShell.run('tensor.py', options, function(err, results) {
+            if (err) throw err;
+            console.log(results[0], 'finished');
+})
+ }
 });
--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import './Index.scss';
-import ImagesUploader from 'react-images-uploader';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 import 'react-images-uploader/styles.css';
 import 'react-images-uploader/font.css';
+import { Meteor } from 'meteor/meteor';
 
-let pictures = ["../../../../public/images/main1.jpg", "../../../../public/images/main2.jpg" ];
-
-let picture = pictures[Math.floor(Math.random() * pictures.length)];
+const CLOUDINARY_UPLOAD_PRESET = 'vptraviu';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dlr2ctpa7/upload';
 
 const loader1 = () => (
   <div class="mult3">
@@ -15,36 +16,80 @@ const loader1 = () => (
       <div class="mult3circle2"></div>
    </div>
   )
-const Index = () => (
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      uploadedFile: null,
+      uploadedFileCloudinaryUrl: ''
+    };
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+     Meteor.call('utility.imageAI', files[0],(err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res)
+  }
+   this.handleImageUpload(files[0]);
+});
+  
+
+  }
+
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                     .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                     .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+render() {
+  return (
   <div id= "main" className="Index">
    
     <h1>Goola</h1>
     <p>An Instagram for food lovers</p>
-    <div>
-         <ImagesUploader
-        url="http://localhost:9090/notmultiple"
-        optimisticPreviews
-        multiple={false}
-        onLoadEnd={(err) => {
-          if (err) {
-            console.error(err);
-          }
-        }}
-        label="Upload a picture"
-        />
-    
-    </div>
+    <form>
+       <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop.bind(this)}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone> 
+     <div>
+        {this.state.uploadedFileCloudinaryUrl === '' ? null :
+        <div>
+          <p>{this.state.uploadedFile.name}</p>
+          <img src={this.state.uploadedFileCloudinaryUrl} />
+        </div>}
+      </div>
+      </form>
     <footer>
       <p>Test it out. Upload a food image and let us classify it for you.</p>
-    </footer>
-      
+    </footer>    
   </div>
-);
+  )
+}
+}
 
 
 
 
 
 
-export default Index;
