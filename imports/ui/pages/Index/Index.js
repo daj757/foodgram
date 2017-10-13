85d5bd14a013be841b07858d47a808d67f6a2764
amberorenstein <amber.orenstein@gmail.com>
2017-10-12 21:16:10 -0600

    image thing sort of might work soon

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,51 +1,61 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './Index.scss';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 import 'react-images-uploader/styles.css';
 import 'react-images-uploader/font.css';
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 
-const CLOUDINARY_UPLOAD_PRESET = 'vptraviu';
-const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dlr2ctpa7/upload';
-
-const loader1 = () => (
-  <div class="mult3">
-      <div class="mult3circle1"></div>
-      <div class="mult3circle2"></div>
-   </div>
-  )
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      owner: "",
       uploadedFile: null,
-      uploadedFileCloudinaryUrl: ''
+      distanceRating: 0,
+      location: "",
+      name: ""
     };
   }
 
-  onImageDrop(files) {
-    this.setState({
-      uploadedFile: files[0]
+  onImageDrop() {
+
+    //    Meteor.call('utility.imageAI', files[0],(err, res) => {
+    // if (err) {
+    //   console.log(err);
+    // } else {
+    //   console.log(res)
+    // }
+    //  this.handleImageUpload(files[0]);
+    Template.Profile.events({
+      'change .myFileInput': function(event, template) {
+        FS.Utility.eachFile(event, function(file) {
+          Images.insert(file, function(err, fileObj) {
+            if (err) {
+              // handle error
+            } else {
+              // handle success depending what you need to do
+              var userId = Meteor.userId();
+              var imagesURL = {
+                "profile.image": " / cfs / files / images / " + fileObj._id
+              };
+              Meteor.users.update(userId, {$set: imagesURL});
+            }
+          });
+        });
+      }
     });
-     Meteor.call('utility.imageAI', files[0],(err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(res)
-  }
-   // this.handleImageUpload(files[0]);
-});
-  
 
   }
+
+  // const imageResponsiveInstance = (
+  //   <Image src="/assets/thumbnail.png" responsive />
+  // );
 
   handleImageUpload(file) {
-    let upload = request.post(CLOUDINARY_UPLOAD_URL)
-                     .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-                     .field('file', file);
+    let upload = request.post(CLOUDINARY_UPLOAD_URL).field('upload_preset', CLOUDINARY_UPLOAD_PRESET).field('file', file);
 
     upload.end((err, response) => {
       if (err) {
@@ -53,43 +63,111 @@ export default class Index extends React.Component {
       }
 
       if (response.body.secure_url !== '') {
-        this.setState({
-          uploadedFileCloudinaryUrl: response.body.secure_url
-        });
+        this.setState({uploadedFileCloudinaryUrl: response.body.secure_url});
       }
     });
   }
-render() {
-  return (
-  <div id= "main" className="Index">
-   
-    <h1>Goola</h1>
-    <p>An Instagram for food lovers</p>
-    <form>
-       <Dropzone
-      multiple={false}
-      accept="image/*"
-      onDrop={this.onImageDrop.bind(this)}>
-      <p>Drop an image or click to select a file to upload.</p>
-    </Dropzone> 
-     <div>
-        {this.state.uploadedFileCloudinaryUrl === '' ? null :
-        <div>
-          <p>{this.state.uploadedFile.name}</p>
-          <img src={this.state.uploadedFileCloudinaryUrl} />
-        </div>}
+  render() {
+    return (
+      <div id="main" className="Index">
+          <input onChange={this.onImageDrop} type="file" name="…" className="myFileInput"/>
       </div>
-      </form>
-    <footer>
-      <p>Test it out. Upload a food image and let us classify it for you.</p>
-    </footer>    
-  </div>
-  )
-}
+    )
+  }
 }
 
-
-
-
-
-
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
+// import './Index.scss';
+// import Dropzone from 'react-dropzone';
+// import request from 'superagent';
+// import 'react-images-uploader/styles.css';
+// import 'react-images-uploader/font.css';
+// import { Meteor } from 'meteor/meteor';
+//
+// const CLOUDINARY_UPLOAD_PRESET = 'vptraviu';
+// const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dlr2ctpa7/upload';
+//
+// const loader1 = () => (
+//   <div class="mult3">
+//       <div class="mult3circle1"></div>
+//       <div class="mult3circle2"></div>
+//    </div>
+//   )
+// export default class Index extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       uploadedFile: null,
+//       uploadedFileCloudinaryUrl: ''
+//     };
+//   }
+//
+//   onImageDrop(files) {
+//     this.setState({
+//       uploadedFile: files[0]
+//     });
+//      Meteor.call('utility.imageAI', files[0],(err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res)
+//   }
+//    // this.handleImageUpload(files[0]);
+// });
+//
+//
+//   }
+//
+//   handleImageUpload(file) {
+//     let upload = request.post(CLOUDINARY_UPLOAD_URL)
+//                      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+//                      .field('file', file);
+//
+//     upload.end((err, response) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//
+//       if (response.body.secure_url !== '') {
+//         this.setState({
+//           uploadedFileCloudinaryUrl: response.body.secure_url
+//         });
+//       }
+//     });
+//   }
+// render() {
+//   return (
+//   <div id= "main" className="Index">
+//
+//     <h1>Goola</h1>
+//     <p>An Instagram for food lovers</p>
+//     <form>
+//        <Dropzone
+//       multiple={false}
+//       accept="image/*"
+//       onDrop={this.onImageDrop.bind(this)}>
+//       <p>Drop an image or click to select a file to upload.</p>
+//     </Dropzone>
+//      <div>
+//         {this.state.uploadedFileCloudinaryUrl === '' ? null :
+//         <div>
+//           <p>{this.state.uploadedFile.name}</p>
+//           <img src={this.state.uploadedFileCloudinaryUrl} />
+//         </div>}
+//       </div>
+//       </form>
+//     <footer>
+//       <p>Test it out. Upload a food image and let us classify it for you.</p>
+//     </footer>
+//   </div>
+//   )
+// }
+// }
+//
+//
+//
+//
+//
+//
--- imports/ui/pages/Terms/Terms.js
+++ imports/ui/pages/Terms/Terms.js
@@ -3,10 +3,11 @@ import "./terms.scss"
 const Terms = () => (
   <div id="terms">
     <h3>Terms and conditions</h3>
-    <hr/>
-    <p>Terms and conditions</p>
-
+    <p>These Terms of Use are effective on October 21, 2017. To access our previous Terms of Use, please click here.</p>
+    <p>By accessing or using the Googla website, the Googla service, or any applications (including mobile applications) made available by Googla (together, the "Service"), however accessed, you agree to be bound by these terms of use ("Terms of Use"). The Service is owned or controlled by Googla, LLC ("Googla"). These Terms of Use affect your legal rights and obligations. If you do not agree to be bound by all of these Terms of Use, do not access or use the Service.</p>
+    <p>There may be times when we offer a special feature that has its own terms and conditions that apply in addition to these Terms of Use. In those cases, the terms specific to the special feature control to the extent there is a conflict with these Terms of Use.</p>
+    <p>ARBITRATION NOTICE: EXCEPT IF YOU OPT-OUT AND EXCEPT FOR CERTAIN TYPES OF DISPUTES DESCRIBED IN THE ARBITRATION SECTION BELOW, YOU AGREE THAT DISPUTES BETWEEN YOU AND Googla WILL BE RESOLVED BY BINDING, INDIVIDUAL ARBITRATION AND YOU WAIVE YOUR RIGHT TO PARTICIPATE IN A CLASS ACTION LAWSUIT OR CLASS-WIDE ARBITRATION.</p>
   </div>
-)
+);
 
 export default Terms
