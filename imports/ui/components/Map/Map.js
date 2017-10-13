2e335fff1c323be601b3b9bedc17a01b188cffd4
dacup72 <dacup72@gmail.com>
2017-10-12 18:46:14 -0600

    added map component

---
+++ imports/ui/components/Map/Map.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { compose, withProps, withStateHandlers } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+
+const MapWithAMakredInfoWindow = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDr94w2Y9dlNUkmnNBm_BmaeGTUaZUDJ7Y&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withStateHandlers(() => ({
+    isOpen: true,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
+    >
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+        <h1>hello</h1>
+      </InfoWindow>}
+    </Marker>
+  </GoogleMap>
+);
+
+export default MapWithAMakredInfoWindow;
