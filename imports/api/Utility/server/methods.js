3fb3770ecd98d954428b3b3ff3c131f2c22762a2
Danny <DJ@MacBook-Pro.local>
2017-10-14 10:58:38 -0600

    pic loader working

--- imports/api/Utility/server/methods.js
+++ imports/api/Utility/server/methods.js
@@ -3,6 +3,8 @@ var path = require("path");
 var exec = Npm.require('child_process').exec;
 var Fiber = Npm.require('fibers');
 var Future = Npm.require('fibers/future');
+var absolutePathToScript = process.argv[1];
+var absolutePathToProject = path.resolve(absolutePathToScript, "../../../..")
 
 import getPrivateFile from '../../../modules/server/get-private-file';
 import parseMarkdown from '../../../modules/parse-markdown';
@@ -13,27 +15,30 @@ Meteor.methods({
     check(fileName, String);
     return parseMarkdown(getPrivateFile(`pages/${fileName}.md`));
   },
-  'utility.imageAI': function utilityImageAI(image) {
-
-//   	console.log("this is it ", path.join(__dirname, 'scrpt1.py'))
-// 	  var options = {
-//     	args: ['squirtle.png']
-// }
+  'utility.imageAI': function utilityImageAI() {
+    var pathToPyScript = path.resolve(absolutePathToProject, 'imports/api/Utility/server/');
+  	console.log("this is it ", pathToPyScript)
+	  var options = {
+      scriptPath: pathToPyScript
+    }
 	
-// 	PythonShell.run(path.join(__dirname, 'script1.py'), options, function(err, results) {
-//             if (err) throw err;
-//             console.log(results[0], 'finished');
+	PythonShell.run("script1.py", options, function(err, results) {
+            if (err) {
+              console.log(err)
+            }
+            else{
+            console.log(results[0], 'finished');
+          }
+})
+  //   exec(path.join(__dirname, 'script1.py'), function (error, stdout, stderr) {
+  //       if (error) {
+  //   console.log(`exec error: ${error}`);
+  //   return;
+  // }
+  // console.log(`stdout: ${stdout}`);
+  // console.log(`stderr: ${stderr}`);
 
-    exec(path.join(__dirname, 'script1.py'), function (error, stdout, stderr) {
-        if (error) {
-    console.log(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-        
-
-  })
-  }
- 
+  // })
+  
+  }      
 });
--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -6,6 +6,7 @@ import request from 'superagent';
 import 'react-images-uploader/styles.css';
 import 'react-images-uploader/font.css';
 import {Meteor} from 'meteor/meteor';
+import Images from '/imports/api/Documents/Images';
 
 export default class Index extends React.Component {
   constructor(props) {
@@ -20,34 +21,43 @@ export default class Index extends React.Component {
     };
   }
 
-  onImageDrop() {
+  onImageDrop(file) {
 
-    //    Meteor.call('utility.imageAI', files[0],(err, res) => {
-    // if (err) {
-    //   console.log(err);
-    // } else {
-    //   console.log(res)
-    // }
-    //  this.handleImageUpload(files[0]);
-    Template.Profile.events({
-      'change .myFileInput': function(event, template) {
-        FS.Utility.eachFile(event, function(file) {
-          Images.insert(file, function(err, fileObj) {
-            if (err) {
-              // handle error
-            } else {
-              // handle success depending what you need to do
-              var userId = Meteor.userId();
-              var imagesURL = {
-                "profile.image": " / cfs / files / images / " + fileObj._id
-              };
-              Meteor.users.update(userId, {$set: imagesURL});
-            }
-          });
-        });
-      }
-    });
+    // _.each(files, function(file) {
+            // file.owner = Meteor.userId();
+            // FS.Utility.eachFile(event, function(file) { 
+        //before upload also save the owner of that file
+            // Images.insert(file, function(err, fileObj) {
+            //     if (err) {
+            //         console.log(err, fileObj); //in case there is an error, log it to the console
+            //     } else {
+            //       console.log("Success image loaded")
+            //         //the image upload is done successfully.
+            //         //you can use this callback to add the id of your file into another collection
+            //         //for this you can use fileObj._id to get the id of the file
+            //     }
+            //   });
+             // });
+        // });
+//      Template.myForm.events({
+//   'change .myFileInput': function(event, template) {
+//     FS.Utility.eachFile(event, function(file) {
+//       Images.insert(file, function (err, fileObj) {
+//         // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+//       });
+//     });
+//   }
+// });
 
+        Meteor.call('utility.imageAI',(err, res) => {
+     if (err) {
+       console.log(err);
+     } else {
+       console.log(res)
+   }
+ })
+  //   //  this.handleImageUpload(files[0]);
+  // })
   }
 
   // const imageResponsiveInstance = (
@@ -69,9 +79,21 @@ export default class Index extends React.Component {
   }
   render() {
     return (
-      <div id="main" className="Index">
-          <input onChange={this.onImageDrop} type="file" name="…" className="myFileInput"/>
-      </div>
+      <div id= "main" className="Index">
+      <div>
+      <h1>Goola</h1>
+    <p>An Instagram for food lovers</p>
+    <form>
+       <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop.bind(this)}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone>
+    </form>
+     </div>
+     </div>
+    
     )
   }
 }
