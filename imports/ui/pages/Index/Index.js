5659f610d23379dd2e2790a2a1f48a050ec0bb37
dacup72 <dacup72@gmail.com>
2017-10-19 20:19:42 -0600

    added grid to index

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -7,11 +7,11 @@ import 'react-images-uploader/styles.css';
 import 'react-images-uploader/font.css';
 import {Meteor} from 'meteor/meteor';
 import Images from '/imports/api/Documents/Images';
+import GridView from '../../components/Grid/Grid';
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       owner: "",
       uploadedFile: null,
@@ -20,22 +20,59 @@ export default class Index extends React.Component {
       name: ""
     };
   }
-
   onImageDrop(files) {
-
- S3.upload({
-        files:files,
-        path:"subfolder"
-      },function(e,r){
-        if(e){
-          console.log(e)
-        }
-        else{
+    S3.upload({
+      files:files,
+      path:"subfolder"
+    },
+    function(e,r) {
+      if(e){
+        console.log(e)
+      } else {
         console.log(r);
       }
     });
+    Meteor.call('utility.imageAI',(err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+      console.log(res)
+      }
+    })
+  }
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL).field('upload_preset', CLOUDINARY_UPLOAD_PRESET).field('file', file);
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+      if (response.body.secure_url !== '') {
+        this.setState({uploadedFileCloudinaryUrl: response.body.secure_url});
+      }
+    });
+  }
+  render() {
+    return (
+      <div id= "main" className="Index">
+        <div>
+          <h1>Goola</h1>
+          <p>An Instagram for food lovers</p>
+          <form>
+            <Dropzone
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onImageDrop.bind(this)}>
+              <p>Drop an image or click to select a file to upload.</p>
+            </Dropzone>
+          </form>
+        </div>
+        <GridView />
+      </div>
+    )
+  }
+}
 
-    // _.each(files, function(file) {
+// _.each(files, function(file) {
             // file.owner = Meteor.userId();
             // FS.Utility.eachFile(event, function(file) {
         //before upload also save the owner of that file
@@ -61,54 +98,17 @@ export default class Index extends React.Component {
 //   }
 // });
 
-        Meteor.call('utility.imageAI',(err, res) => {
-     if (err) {
-       console.log(err);
-     } else {
-       console.log(res)
-   }
- })
-  //   //  this.handleImageUpload(files[0]);
-  // })
-  }
 
-  // const imageResponsiveInstance = (
+//   //  this.handleImageUpload(files[0]);
+  // })
+
+// const imageResponsiveInstance = (
   //   <Image src="/assets/thumbnail.png" responsive />
   // );
 
-  handleImageUpload(file) {
-    let upload = request.post(CLOUDINARY_UPLOAD_URL).field('upload_preset', CLOUDINARY_UPLOAD_PRESET).field('file', file);
 
-    upload.end((err, response) => {
-      if (err) {
-        console.error(err);
-      }
 
-      if (response.body.secure_url !== '') {
-        this.setState({uploadedFileCloudinaryUrl: response.body.secure_url});
-      }
-    });
-  }
-  render() {
-    return (
-      <div id= "main" className="Index">
-      <div>
-      <h1>Goola</h1>
-    <p>An Instagram for food lovers</p>
-    <form>
-       <Dropzone
-      multiple={false}
-      accept="image/*"
-      onDrop={this.onImageDrop.bind(this)}>
-      <p>Drop an image or click to select a file to upload.</p>
-    </Dropzone>
-    </form>
-     </div>
-     </div>
 
-    )
-  }
-}
 
 // import React from 'react';
 // import { Button } from 'react-bootstrap';
